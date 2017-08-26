b523c51a5d1dd57735d3127cf08c71c241046441
nixaun <lenn_peeters@yahoo.com>
2017-08-26 18:08:58 +0200

    moving left right and down

--- game/js/myscript.js
+++ game/js/myscript.js
@@ -41,7 +41,7 @@ let lastTime = 0;
 function update(time = 0){
 	const deltaTime = time - lastTime;
 	lastTime = time;
-	
+
 	dropCounter += deltaTime;
 	if(dropCounter > dropInterval){
 		player.pos.y++;
@@ -55,5 +55,18 @@ const player = {
 	pos: {x: 5, y: 5},
 	matrix: matrix,
 }
+
+document.addEventListener('keydown', event => {
+	if(event.keyCode === 37){
+		player.pos.x--;
+	}
+	else if(event.keyCode === 39){
+		player.pos.x++;
+	}
+	else if(event.keyCode === 40){
+		player.pos.y++;
+		dropCounter = 0;
+	}
+});
 
 update();
